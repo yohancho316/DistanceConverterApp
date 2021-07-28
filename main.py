58eafdb8cabe1523f3782f653cce36829f69e9f8
John Cho <yohancho316@gmail.com>
2021-07-27 20:35:59 -0700

    Implemented FeetToMetres Class

--- main.py
+++ main.py
@@ -43,6 +43,33 @@ class MetresToFeet(ttk.Frame):
     except ValueError:
       pass
 
+# Feet to Metres Class
+class FeetToMetres(ttk.Frame):
+  def __init__(self, container):
+      super().__init__(container)
+      self.feet_value = tk.StringVar()
+      self.metres_value = tk.StringVar()
+      feet_label = ttk.Label(self,text='Feet:')
+      feet_label.grid(column=1,row=1,sticky='W',ipadx=5)
+      feet_input = ttk.Entry(self,width=10,textvariable=self.feet_value)
+      feet_input.grid(column=2,row=1,sticky='EW')
+      feet_input.focus()
+      metres_label = ttk.Label(self,text='Metres:')
+      metres_label.grid(column=1,row=2,sticky='W',ipadx=5)
+      metres_display = ttk.Label(self,textvariable=self.metres_value)
+      metres_display.grid(column=2,row=2,sticky='EW')
+      calculate_button = ttk.Button(self,text='Calculate:',command=self.calculate_metres)
+      calculate_button.grid(column=1,row=3,columnspan=2,sticky='EW')
+      for child in self.winfo_children():
+          child.grid_configure(padx=5,pady=5)
+
+  def calculate_metres(self,*args):
+    try:
+      value = float(self.feet_value.get())
+      self.feet_value.set('%.2f' % (value * 3.28084))
+    except ValueError:
+      pass
+
 ################################# Python Executible Code ######################################
 root = DistanceConverter()
 root.mainloop()
