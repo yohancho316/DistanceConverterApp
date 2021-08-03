61c86ae3c104cf675f23aa7da06721fcc5817a86
John Cho <yohancho316@gmail.com>
2021-08-03 14:58:53 -0700

    Doc Edit & Code Edit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # DistanceConverterApp
+
+# Table of Contents
+* Project Description
+* Getting Started
+* Prerequisites
+* Installation & Deployment
+
+
+## Project Description
+
+This is a distance converter application written in Python with a GUI frontend designed in TK inter. This application is capable of converting feet to meters and meters to feet by pressing a button within the app.
+
+```
+Click on the Image to Watch My Project in Action on YouTube
+```
+[![Watch my Project in Action on YouTube](./images/smiley_cube_image.png)](https://www.youtube.com/watch?v=jEK_BHXmWkg)
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Prerequisites
+
+This Python project uses the following external Python libraries that must be installed via Pip or via requirements.txt.
+
+* TKinter
+* Python
+
+## Installation & Deployment
+
+A step by step guide that will tell you how to get the development environment up and running.
+
+Step 1: Clone the repository:
+```
+https://github.com/yohancho316/DisplaySmileyFaces.git
+```
+Step 2: Use the given command to install project dependencies via Pip:
+```
+pip install -r requirements.txt
+```
+Step 3: In terminal navigate to the project's local directory and run this command to run the application:
+```
+python3 main.py
+```
+
+
+
--- main.py
+++ main.py
@@ -19,12 +19,12 @@ class DistanceConverter(tk.Tk):
     style.theme_use('classic')
 
     # Create Dictionary of Frame KVP
-    for FrameClass in (MetresToFeet,FeetToMetres):
+    for FrameClass in (MetersToFeet,FeetToMeters):
       frame = FrameClass(container,self)
       self.frames[FrameClass] = frame
       frame.grid(row=0,column=0,sticky="NSEW")
 
-    self.show_frame(MetresToFeet)
+    self.show_frame(MetersToFeet)
 
 # Decide Which Frame to Display First
   def show_frame(self,container):
@@ -34,29 +34,29 @@ class DistanceConverter(tk.Tk):
     frame.tkraise()
 
 # Meters to Feet Class
-class MetresToFeet(ttk.Frame):
+class MetersToFeet(ttk.Frame):
   def __init__(self, container,controller,**kwargs):
     super().__init__(container,**kwargs)
     self.feet_value = tk.StringVar()
-    self.metres_value = tk.StringVar()
+    self.meters_value = tk.StringVar()
 
     # Label Widgets
-    metres_label = ttk.Label(self,text='Metres:')
-    metres_label.grid(row=1,column=1,sticky='W',ipadx=5)
+    meters_label = ttk.Label(self,text='Meters:')
+    meters_label.grid(row=1,column=1,sticky='W',ipadx=5)
     feet_label = ttk.Label(self,text='Feet:')
     feet_label.grid(row=2,column=1,sticky='W',ipadx=5)
     feet_display = ttk.Label(self,textvariable=self.feet_value)
     feet_display.grid(row=2,column=2,sticky='WE')
 
     # Entry Widget
-    metres_input = ttk.Entry(self,width=10,textvariable=self.metres_value)
-    metres_input.grid(row=1,column=2,sticky='WE')
-    metres_input.focus()
+    meters_input = ttk.Entry(self,width=10,textvariable=self.meters_value)
+    meters_input.grid(row=1,column=2,sticky='WE')
+    meters_input.focus()
 
     # Button Widgets
     calculate_button = ttk.Button(self,text='Calculate:',command=self.calculate)
     calculate_button.grid(row=3,column=0,columnspan=2,sticky='WE')
-    switch_page_button = ttk.Button(self,text='Switch Feet to Metres Calc',command=lambda: controller.show_frame(FeetToMetres))
+    switch_page_button = ttk.Button(self,text='Switch Feet to Meters Calc',command=lambda: controller.show_frame(FeetToMeters))
     switch_page_button.grid(row=4,column=0,columnspan=2,sticky='WE')
 
     for child in self.winfo_children():
@@ -64,25 +64,25 @@ class MetresToFeet(ttk.Frame):
 
   def calculate(self,*args):
     try:
-      value = float(self.metres_value.get())
+      value = float(self.meters_value.get())
       self.feet_value.set('%.2f' % (value * 3.28084))
     except ValueError:
       pass
 
-# Feet to Metres Class
-class FeetToMetres(ttk.Frame):
+# Feet to Meters Class
+class FeetToMeters(ttk.Frame):
   def __init__(self,container,controller,**kwargs):
     super().__init__(container,**kwargs)
     self.feet_value = tk.StringVar()
-    self.metres_value = tk.StringVar()
+    self.meters_value = tk.StringVar()
 
     # Label Widgets
     feet_label = ttk.Label(self,text='Feet:')
     feet_label.grid(row=1,column=1,sticky='W',ipadx=5)
-    metres_label = ttk.Label(self,text='Metres:')
-    metres_label.grid(row=2,column=1,sticky='W',ipadx=5)
-    metres_display = ttk.Label(self,textvariable=self.metres_value)
-    metres_display.grid(row=2,column=2,sticky='WE')
+    meters_label = ttk.Label(self,text='Meters:')
+    meters_label.grid(row=2,column=1,sticky='W',ipadx=5)
+    meters_display = ttk.Label(self,textvariable=self.meters_value)
+    meters_display.grid(row=2,column=2,sticky='WE')
 
     # Entry Widgets
     feet_input = ttk.Entry(self,width=10,textvariable=self.feet_value)
@@ -92,7 +92,7 @@ class FeetToMetres(ttk.Frame):
     # Button Widgets
     calculate_button = ttk.Button(self,text='Calculate:',command=self.calculate)
     calculate_button.grid(row=3,column=0,columnspan=2,sticky='WE')
-    switch_page_button = ttk.Button(self,text='Switch Metres to Feet Calc',command=lambda: controller.show_frame(MetresToFeet))
+    switch_page_button = ttk.Button(self,text='Switch Meters to Feet Calc',command=lambda: controller.show_frame(MetersToFeet))
     switch_page_button.grid(row=4,column=0,columnspan=2,sticky='WE')
 
     for child in self.winfo_children():
@@ -101,7 +101,7 @@ class FeetToMetres(ttk.Frame):
   def calculate(self,*args):
     try:
       value = float(self.feet_value.get())
-      self.metres_value.set('%.2f' % (value * 3.28084))
+      self.meters_value.set('%.2f' % (value * 3.28084))
     except ValueError:
       pass
 
